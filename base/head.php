07154ee3655127c99011d9c23f9f827d075a2356
3A732087 <quannn0180716v@gmail.com>
2022-02-15 23:03:20 +0800

    css檔案位置調整、新增後台head.php檔

---
+++ base/head.php
@@ -0,0 +1,23 @@
+<!-- Bootstrap Core CSS -->
+<link href="../css/back-end/bootstrap.min.css" rel="stylesheet">
+
+<!-- Custom CSS -->
+<link href="../css/back-end/sb-admin.css" rel="stylesheet">
+
+<!-- Morris Charts CSS -->
+<link href="../css/back-end/plugins/morris.css" rel="stylesheet">
+
+<!-- Custom Fonts -->
+<link href="../css/back-end/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+<!--[if lt IE 9]>
+<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+<script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+<script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js"
+        integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+"
+        crossorigin="anonymous"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
