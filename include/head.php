d55c8508ab4e2d5a5f42c1796d23fb882bfd24e2
3A732087 <quannn0180716v@gmail.com>
2022-02-17 23:05:30 +0800

    head, nav-bar共通部分調整

---
+++ include/head.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include_once 'database.php'; ?>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title><?php echo ($title); ?></title>
+    <link rel="shortcut icon" href="favicon.ico" />
+
+    <!-- FACEBOOK SEO -->
+    <meta property="og:title" content="漢修學苑" />
+    <meta property="og:url" content="https://hanhsiu.org/" />
+    <meta property="og:image" content="img/fb-open-graph-1.jpg" />
+    <meta property="og:type" content="article" />
+    <meta property="og:description" content="《瑜伽師地論》線上筆記" />
+    <meta property="og:locale" content="zh_tw" />
+
+    <!-- OTHER SETTING -->
+    <meta name="format-detection" content="telephone=no" />
+    <meta name="theme-color" content="#70593e" />
+
+    <!-- STYLE -->
+    <link rel="stylesheet" href="css/front-end/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/front-end/tiny-slider.css" />
+    <link rel="stylesheet" href="css/front-end/animate.min.css" />
+    <link rel="stylesheet" href="css/front-end/icofont/style.css" />
+    <link rel="stylesheet" href="css/front-end/main.css" />
+
+    <!-- JAVASCRIPT -->
+    <script src="js/w3.js"></script>
+</head>
