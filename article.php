7d960dcc4d0ea3649dc04e5f0caaa789de293343
3A732087 <quannn0180716v@gmail.com>
2022-03-05 00:52:01 +0800

    瑜論講記(搜尋功能未寫)

---
+++ article.php
@@ -0,0 +1,169 @@
+<?php include_once 'database.php'; ?>
+<?php $title = '瑜論講記'; ?>
+<?php include_once 'include/head.php'; ?>
+<?php include_once 'include/nav-bar.php'; ?>
+
+<body>
+    <main class="article-mode">
+        <section>
+            <div class="container-lg main-wrap">
+                <div class="main-wrap__left">
+                    <div data-boxname="head" class="heading-style__1 mx-auto mb-5">
+                        瑜論講記
+                        <div class="tit-en">lecture</div>
+                    </div>
+
+                    <div class="article-mode__ctrl">
+                        <select class="form-select">
+                            <option value="初發論端">初發論端</option>
+                            <option value="本地分" selected>本地分</option>
+                            <option value="攝決擇分">攝決擇分</option>
+                            <option value="攝釋分">攝釋分</option>
+                            <option value="攝異門分">攝異門分</option>
+                            <option value="攝事分">攝事分</option>
+                            <option value="連結檔">連結檔</option>
+                        </select>
+
+                        <select class="form-select">
+                            <option value="1" selected>01</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                            <option value="2">02</option>
+                        </select>
+
+                        <button type="submit" class="btn-style__1 smaller full-w" disabled>上一卷</button>
+                        <button type="submit" class="btn-style__1 smaller full-w">下一卷</button>
+                    </div>
+                </div>
+
+                <?php
+                if (isset($_GET["sid"])) {
+                    $sqltit = "Select * From `scripture` where `s_id`= $_GET[sid]";
+                    $tit = "";
+                    $resultshow[$tit] = mysqli_query($db_link, $sqltit);
+                    $rtit = mysqli_fetch_row($resultshow[$tit]);
+                    $sqltype = "SELECT s_id , scripture.typename , number , title , filename , date FROM `scripture` ,`types` WHERE `scripture`.`s_id`=$_GET[sid] AND `types`.`t_id`=$rtit[1]";
+                    $resulttype = mysqli_query($db_link, $sqltype);
+                    $type = mysqli_fetch_row($resulttype);
+                    $sql_count = "update  scripture set clicktimes =clicktimes+1  WHERE scripture.s_id = $_GET[sid]";
+                    mysqli_query($db_link, $sql_count);
+                    $sqlcount = "SELECT * FROM scripture  WHERE scripture.s_id = $_GET[sid] ";
+                    $resultcount = mysqli_query($db_link, $sqlcount);
+                    $rowcount = mysqli_fetch_assoc($resultcount);
+                ?>
+
+                    <div class="main-wrap__right">
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb justify-content-end">
+                                <li class="breadcrumb-item">
+                                    <a href="articletype.html">瑜論講記</a>
+                                </li>
+                                <li class="breadcrumb-item">
+                                    <a href="articlepages.html">
+                                        <?php echo "$type[1]"; ?>
+                                    </a>
+                                </li>
+                            </ol>
+                        </nav>
+
+                        <!-- 文章標題 -->
+                        <div class="d-flex flex-column flex-md-row justify-content-md-between align-items-md-center mb-4">
+                            <h4 class="heading-style__2 mb-2 mb-md-0">
+                                <?php echo "$type[1]"; ?>
+                                <span class="tline">
+                                    <?php echo "$type[2]"; ?>
+                                </span>
+                            </h4>
+                            <p class="mb-0 color02">瀏覽人次為
+                                <span class="ms-2">
+                                <?php echo "$rowcount[clicktimes]";
+                            }
+                                ?>
+                                </span>
+                            </p>
+                        </div>
+
+                        <!-- 文章內文 -->
+                        <article>
+                            <?php
+                            $_SESSION["tid"] = "$rtit[0]";
+
+                            $typename = $rtit[2];
+                            $filename = $rtit[5];
+                            $str = "";
+                            //判斷是否有該檔案
+                            if (file_exists("./ScriptureFile/$typename/$filename")) {
+                                $file = fopen("./ScriptureFile/$typename/$filename", "r");
+                                if ($file != NULL) {
+                                    //當檔案未執行到最後一筆，迴圈繼續執行(fgets一次抓一行)
+                                    while (!feof($file)) {
+                                        $str .= fgets($file);
+                                    }
+                                    fclose($file);
+                                }
+                            }
+                            echo "$str";
+                            ?>
+                        </article>
+                    </div>
+            </div>
+        </section>
+
+        <div class="wave-block in-buttom"></div>
+    </main>
+
+    <footer w3-include-html="include/_footer.html"></footer>
+
+    <!-- JAVASCRIPT W3 -->
+    <script>
+        w3.includeHTML();
+    </script>
+
+    <!-- JAVASCRIPT -->
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/tiny-slider.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
---
+++ articletype.php
@@ -0,0 +1,183 @@
+<?php include_once 'database.php'; ?>
+<?php $title = '瑜論講記'; ?>
+<?php include_once 'include/head.php'; ?>
+<?php include_once 'include/nav-bar.php'; ?>
+
+<body>
+    <main>
+        <div class="hero stop-banner">
+            <svg class="cloud cleft">
+                <use xlink:href="img/cloud_left.svg#cloud-left"></use>
+            </svg>
+            <svg class="cloud cright">
+                <use xlink:href="img/cloud_right.svg#cloud-right"></use>
+            </svg>
+        </div>
+
+        <section>
+            <div class="container main-wrap">
+                <div class="main-wrap__left">
+                    <div data-boxname="head" class="heading-style__1 mx-auto mb-5">
+                        瑜論講記
+                        <div class="tit-en">lecture</div>
+                    </div>
+
+                    <form action="articletype_sr.html">
+                        <div class="close-wrap site1">
+                            <button type="button" class="btn-style__arrow">
+                                <i class="arrow"></i>
+                            </button>
+                            <div class="mb-3">
+                                <label class="col-form-label">搜尋 / Search</label>
+                                <input type="text" class="form-control" placeholder="請輸入文字" />
+                            </div>
+                            <div class="d-flex mb-3">
+                                <div class="form-check me-4">
+                                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" checked />
+                                    <label class="form-check-label" for="flexRadioDefault1"> 標題 </label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
+                                    <label class="form-check-label" for="flexRadioDefault2"> 內容 </label>
+                                </div>
+                            </div>
+                            <input type="submit" class="btn-style__1 smaller full-w" value="搜尋" />
+                        </div>
+                    </form>
+                </div>
+
+                <!--  **********  判斷是否點選講記類別  **************-->
+                <?php
+                if (isset($_GET["tid"])) {
+                    $tid = $_GET["tid"];
+                    $sqltype = "SELECT * FROM `types` where `t_id` = $tid";
+                    $resulttype = mysqli_query($db_link, $sqltype);
+                    $rtypename = mysqli_fetch_row($resulttype);
+                    $_SESSION['rtypename'] = $rtypename[1]; ?>
+
+                    <!--  **********  已點選講記類別  **************-->
+                    <div class="main-wrap__right">
+                        <!-- 選擇單元 -->
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb justify-content-end">
+                                <li class="breadcrumb-item">
+                                    <a href="articletype.html">瑜論講記</a>
+                                </li>
+                                <li class="breadcrumb-item active" aria-current="page">
+                                    <?php echo "$rtypename[2]" ?>
+                                </li>
+                            </ol>
+                        </nav>
+
+                        <div class="mb-4">
+                            <h4 class="heading-style__2">
+                                <?php echo "$rtypename[2]" ?>
+                            </h4>
+                        </div>
+
+                        <div class="page-wrap">
+                            <?php
+                            $sqlatcnum = "SELECT * FROM `scripture` where  `save`='0' && `t_id` = $tid ";
+
+                            $result_row = mysqli_query($db_link, $sqlatcnum);
+                            $data = mysqli_num_rows($result_row);       //抓總共幾筆
+                            $per = 10;
+                            $rows = ceil($data / $per);
+
+                            $resultnum = mysqli_query($db_link, $sqlatcnum);
+
+                            for ($i = 1; $i <= $rows; $i++) {
+                                $start = ($i - 1) * 10;
+                                $sqlatcnum10 = "SELECT * FROM `scripture` where  `save`='0' &&`t_id` = $tid order by CAST(`number` AS UNSIGNED) ASC,s_id ASC Limit  $start  , $per";
+                                $resultnum10 = mysqli_query($db_link, $sqlatcnum10);
+                                while ($script = mysqli_fetch_assoc($resultnum10)) {
+                                    echo "<a href=article.php?sid='$script[s_id]' title='$script[number]'>$script[number]</a>";
+                                }
+                            } ?>
+                        </div>
+
+                        <div class="text-center my-5">
+                            <a class="btn-style__1" href="javascript:window.history.go(-1);">回上一頁</a>
+                        </div>
+                    </div>
+
+                <?php
+                } else {
+                ?>
+
+                    <!--  **********  未點選講記類別  **************-->
+                    <div class="main-wrap__right">
+                        <!-- 選擇項目 -->
+                        <ul class="book-wrap mb-4">
+                            <?php
+                            $sqlatypecnum = "SELECT * FROM `types` order by listorder";
+                            $results_row = mysqli_query($db_link, $sqlatypecnum);
+                            $datas = mysqli_num_rows($results_row);       //抓總共幾筆
+                            $per = 10;
+                            $rows = ceil($datas / $per);
+                            $resultsnum = mysqli_query($db_link, $sqlatypecnum);
+                            $sqlscr = "SELECT * FROM `scr_show` ";
+                            $result = mysqli_query($db_link, $sqlscr);
+                            $rowscr = mysqli_fetch_assoc($result);
+                            if ($_SESSION['authority'] == '1' || $_SESSION['authority'] == '2') { //還未編輯之處理
+                                for ($j = 1; $j <= $rows; $j++) {
+                                    $start = ($j - 1) * 10;
+                                    $sqlatcnums10 = "SELECT * FROM types order by listorder Limit $start , $per";
+                                    $resultnums10 = mysqli_query($db_link, $sqlatcnums10);
+
+                                    while ($row = mysqli_fetch_assoc($resultnums10)) {
+                                        echo "<li>";
+                                        echo "<a class='book-wrap__link' href=?tid='$row[t_id]' title='$row[typename]'`>";
+                                        echo "<div class='book-wrap__front'>";
+                                        echo "<span>$row[typename]</span>";
+                                        echo "</div>";
+                                        echo "</a>";
+                                        echo "</li>";
+                                    }
+                                }
+                            } else {
+                                if ($rowscr['shownumber'] == '0') {
+                                    echo "<center> <font color=#612E04><h1>※網頁尚在構置中※<h1></font></center>";
+                                } else {
+                                    for ($j = 1; $j <= $rows; $j++) {
+                                        $start = ($j - 1) * 10;
+                                        $sqlatcnums10 = "SELECT * FROM types order by listorder Limit $start , $per";
+                                        $resultnums10 = mysqli_query($db_link, $sqlatcnums10);
+
+                                        while ($row = mysqli_fetch_assoc($resultnums10)) {
+                                            echo "<li>";
+                                            echo "<a class='book-wrap__link' href=?tid='$row[t_id]' title='$row[typename]'`>";
+                                            echo "<div class='book-wrap__front'>";
+                                            echo "<span>$row[typename]</span>";
+                                            echo "</div>";
+                                            echo "</a>";
+                                            echo "</li>";
+                                        }
+                                    }
+                                }
+                            } ?>
+                        </ul>
+                    </div>
+                <?php
+                }
+                ?>
+            </div>
+        </section>
+
+        <div class="wave-block in-buttom"></div>
+    </main>
+
+    <footer w3-include-html="include/_footer.php"></footer>
+
+    <!-- JAVASCRIPT W3 -->
+    <script>
+        w3.includeHTML();
+    </script>
+
+    <!-- JAVASCRIPT -->
+    <script src="js/bootstrap.bundle.min.js"></script>
+    <script src="js/tiny-slider.js"></script>
+    <script src="js/main.js"></script>
+</body>
+
+</html>
