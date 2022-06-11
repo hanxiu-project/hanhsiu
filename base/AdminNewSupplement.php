1f51e830a8bf4823e0b2e2255c4d9570b2d8a63a
DESKTOP-3CLUO3B\USER <2365275a@gmail.com>
2022-06-11 17:42:20 +0800

    超多東西

--- base/AdminNewSupplement.php
+++ base/AdminNewSupplement.php
@@ -35,7 +35,9 @@ include 'verification.php';
             <div class="col-lg-12">
                 <label for="content" style="color:#ffffff" ><b >新增補充資料類別:</b></label>
                 <input id="type" name="type" type="text" style="width:525px; height:30px; color:#000000; ">
-                <input type="submit" class="btn btn-sm btn-warning" name="go" value="新增">
+                <input type="submit" class="btn btn-sm btn-warning" name="go" value="新增"><br>
+                <label for="content" style="color:#ffffff" ><b >補充資料類別之註解:</b></label>
+                <input id="type" name="memo" type="text" style="width:525px; height:30px; color:#000000; ">
             </div>
         </div>
 
@@ -58,12 +60,17 @@ echo "<form name='form1' method='POST' action=''>";
 echo "<table border rules=rows cellspacing=0 width=100% style=font-size:20px;line-height:50px;>";
 echo "<tr align=center>";
 echo "<td><b>補充資料類別</b></td>";
+echo "<td><b>類別備註</b></td>";
 echo "<td></td>";
 echo "</tr>";
 
 while ($row = $result->fetch_assoc()) {
     echo "<tr align=center>";
     echo "<td>$row[spmtypename]</td>";
+   
+    echo "<td><input id=type name='$row[spt_id]+3' type=text value='$row[notes]'style='width:700px';>";
+    echo "<input type='submit' class='btn btn-sm btn-danger ' value='更改' style='width:100px;height:30px;'name='$row[spt_id]+4' ;></td>";
+    echo "</td>";
     echo "<td>";
     ?>
                         <input type='submit' class="btn btn-sm btn-danger " style='width:100px;height:30px;'
@@ -83,7 +90,7 @@ $max_listorder = $listcheck['max'];
 
 while ($row2 = $result2->fetch_assoc()) {
     if (isset($_POST["$row2[spt_id]+2"])) {
-        $file_path = "supplement/$row2[spmtypename]";
+        $file_path = "../supplement/$row2[spmtypename]";
         if (is_dir($file_path)) { //先判斷是不是資料夾
             if (rmdir($file_path)) { //判斷是否能刪除成功
                 //                                    echo “刪除資料夾成功”;
@@ -101,19 +108,33 @@ while ($row2 = $result2->fetch_assoc()) {
         }
     }
 }
+$sql3 = "SELECT * FROM spm_types ";
+$result3 = mysqli_query($db_link, $sql3);
+$sql_listorder3 = " SELECT MAX(listorder) as max FROM  `spm_types` ";
+$listresult3 = mysqli_query($db_link, $sql_listorder3);
+$listcheck3 = mysqli_fetch_assoc($listresult3);
+$max_listorder = $listcheck3['max'];
+while ($row3 = $result3->fetch_assoc()) {
+    if (isset($_POST["$row3[spt_id]+4"])) {
+        $memo3=$_POST["$row3[spt_id]+3"];
+        $sqliit = "UPDATE `spm_types` set `notes`= '$memo3'  where `spt_id` = '$row3[spt_id]'";
+        mysqli_query($db_link, $sqliit);
+        echo "<script>alert('更改成功');location.href='AdminNewSupplement.php'</script>";
+    }
+}
 
 $type = $_POST["type"]; //新增講記類別
-
+$memo = $_POST["memo"];
 if (isset($_POST["go"])) {
-    if ($type == null) {
-        echo "<script>alert('請輸入欲新增的類別!');location.href='AdminNewSupplement.php'</script>";
+    if ($type == null || $memo == null) {
+        echo "<script>alert('請輸入欲新增的類別及備註!');location.href='AdminNewSupplement.php'</script>";
     } else {
         //資料夾的建立
-        $file_path = "./supplement/$type";
+        $file_path = "../supplement/$type";
         if (!file_exists($file_path)) {
             mkdir($file_path);
             //echo “建立資料夾成功”;
-            $sqltype = "INSERT INTO spm_types (spmtypename,listorder) VALUES ('$type','$max_listorder'+1)";
+            $sqltype = "INSERT INTO spm_types (spmtypename,notes,listorder) VALUES ('$type','$memo','$max_listorder'+1)";
             mysqli_query($db_link, $sqltype);
             echo "<script>alert('補充資料類別建立成功!');location.href='AdminNewSupplement.php'</script>";
         } else {
@@ -122,6 +143,7 @@ if (isset($_POST["go"])) {
         }
     }
 }
+
 
 mysqli_close($db_link);
 ?>
