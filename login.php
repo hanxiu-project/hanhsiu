5686cad3e5dbbdd71d05eaf2b709446a9a191a04
Tony Hu <benabcd5779@gmail.com>
2022-06-21 16:17:26 +0800

    google recaptcha功能修改

--- login.php
+++ login.php
@@ -5,7 +5,8 @@
 <!-- STYLE -->
 
 <head>
-    <script src='https://www.google.com/recaptcha/api.js'></script>
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+</head>
 
 <body>
     <main>
@@ -28,7 +29,7 @@
                                 <label for="floatingPassword">密碼 / Password</label>
                             </div>
                             <div class="mb-3 d-flex justify-content-center">
-                                <div class="g-recaptcha" data-sitekey="6Lf2IyUgAAAAAHcP8VEbVsTnUXBkTiPGleznYRWx"></div>
+                                <div class="g-recaptcha" data-sitekey="6Le7gYUgAAAAAJn5qk84x7zwD3-90DQj88xW8R48"></div>
                             </div>
 
                             <input type="submit" name="gologin" class="btn-style__1 full-w" value="登入" />
@@ -64,28 +65,40 @@
         $_SESSION["verified"] = $row["verified"];
 
         if (isset($_POST["gologin"])) {
-            if ($_POST["account"] == null || $_POST["password"] == null) {
-                echo "<script>alert('請輸入帳號或密碼！');location.href='login.php'</script>";
-            } else if ($acc != $_POST["account"] || (!password_verify($_POST["password"], $pwdhash))) {
-                echo "<script>alert('帳號或密碼錯誤！請重新輸入。');location.href='login.php'</script>";
-            }
-            /*else if ($_SESSION["verified"] == 0) {
-            echo "<script>alert('帳號尚未驗證，請至email收取信驗證信。');location.href='login.php'</script>";
-
-=======
-        } */ else if ($authority == 0) {
-                $_SESSION['acc'] = $_POST["account"];                            //session存帳號密碼
-                $_SESSION['pwd'] = $pwdhash;
-                //header("location:indexs.php");
-                echo "<script>location.href='news.php';</script>";        //導向一般會員頁面
-            } else if ($authority == 1 || $authority == 2) {
-                $_SESSION['acc'] = $_POST["account"];                            //session存帳號密碼
-                $_SESSION['pwd'] = $pwdhash;
-                $_SESSION["updatename"] = "$row[name]";
-                //header("location:AdminScriptureManage.php");
-                echo "<script>location.href='base/AdminScriptureManage.php';</script>";        //導向管理員頁面
-
-
+            if (isset($_POST['g-recaptcha-response'])) {
+                $secretkey = "6Le7gYUgAAAAAAllW_cx06Pn-wu3TJxc7mNL9Z_9";
+                $ip = $_SERVER['REMOTE_ADDR'];
+                $response = $_POST['g-recaptcha-response'];
+                $url = "https://www.google.com/recaptcha/api/siteverify?secret=$secretkey&response=$response&remoteip=$ip";
+                $fire = file_get_contents($url);
+                $data = json_decode($fire);
+                if ($data->success == true) {
+                    if ($_POST["account"] == null || $_POST["password"] == null) {
+                        echo "<script>alert('請輸入帳號或密碼！');location.href='login.php'</script>";
+                    } else if ($acc != $_POST["account"] || (!password_verify($_POST["password"], $pwdhash))) {
+                        echo "<script>alert('帳號或密碼錯誤！請重新輸入。');location.href='login.php'</script>";
+                    }
+                    /*else if ($_SESSION["verified"] == 0) {
+                    echo "<script>alert('帳號尚未驗證，請至email收取信驗證信。');location.href='login.php'</script>";
+        
+        =======
+                } */ else if ($authority == 0) {
+                        $_SESSION['acc'] = $_POST["account"];                            //session存帳號密碼
+                        $_SESSION['pwd'] = $pwdhash;
+                        //header("location:indexs.php");
+                        echo "<script>location.href='news.php';</script>";        //導向一般會員頁面
+                    } else if ($authority == 1 || $authority == 2) {
+                        $_SESSION['acc'] = $_POST["account"];                            //session存帳號密碼
+                        $_SESSION['pwd'] = $pwdhash;
+                        $_SESSION["updatename"] = "$row[name]";
+                        //header("location:AdminScriptureManage.php");
+                        echo "<script>location.href='base/AdminScriptureManage.php';</script>";        //導向管理員頁面
+                    }
+                } else {
+                    echo "<script>alert('驗證失敗，請重新嘗試！');</script>";
+                }
+            } else {
+                echo "<script>alert('驗證功能錯誤，請重新嘗試！');</script>";
             }
         }
         ?>
