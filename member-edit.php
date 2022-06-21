0be236fbb2f0e5ab558ae37e04a033f48a55e26f
Tony Hu <benabcd5779@gmail.com>
2022-06-21 21:02:47 +0800

    會員基本資料修改  功能fix

--- member-edit.php
+++ member-edit.php
@@ -23,18 +23,18 @@
                     <div class="mb-3 row">
                         <label for="inputName" class="col-sm-5 col-lg-3 col-form-label">姓名 / Ｎame</label>
                         <div class="col-sm-7 col-lg-9">
-                            <input type="text" class="form-control" id="inputName" name="name" placeholder="請輸入姓名" />
+                            <input type="text" class="form-control" id="inputName" name="name" value="<?php echo $row[name] ?>" placeholder="請輸入姓名" />
                         </div>
                     </div>
                     <div class="mb-3 row">
                         <label for="inputName" class="col-sm-5 col-lg-3 col-form-label">性別 / Ｇender</label>
                         <div class="col-sm-7 col-lg-9 d-flex">
                             <div class="form-check me-4">
-                                <input class="form-check-input" type="radio" name="sex" id="flexRadioDefault1" />
+                                <input class="form-check-input" type="radio" name="sex" value="男" id="flexRadioDefault1" <?php if($row[gender] == "男") echo "checked" ?> />
                                 <label class="form-check-label" for="flexRadioDefault1">男</label>
                             </div>
                             <div class="form-check">
-                                <input class="form-check-input" type="radio" name="sex" id="flexRadioDefault2" checked />
+                                <input class="form-check-input" type="radio" name="sex" value="女" id="flexRadioDefault2" <?php if($row[gender] == "女") echo "checked" ?> />
                                 <label class="form-check-label" for="flexRadioDefault2">女</label>
                             </div>
                         </div>
@@ -42,19 +42,19 @@
                     <div class="mb-3 row">
                         <label for="inputEmail" class="col-sm-5 col-lg-3 col-form-label">電子信箱 / E-mail</label>
                         <div class="col-sm-7 col-lg-9">
-                            <input type="email" class="form-control" id="inputEmail" name="email" placeholder="請輸入電子信箱" />
+                            <input type="email" class="form-control" id="inputEmail" name="email" value="<?php echo $row[email] ?>" placeholder="請輸入電子信箱" />
                         </div>
                     </div>
                     <div class="mb-3 row">
                         <label for="inputPhone" class="col-sm-5 col-lg-3 col-form-label">手機號碼 / Phone</label>
                         <div class="col-sm-7 col-lg-9">
-                            <input type="tel" class="form-control" id="inputPhone" name="telephone" placeholder="請輸入手機號碼" />
+                            <input type="tel" class="form-control" id="inputPhone" name="telephone" value="<?php echo $row[telephone] ?>" placeholder="請輸入手機號碼" />
                         </div>
                     </div>
                     <div class="mb-5 row">
                         <label for="inputAddress" class="col-sm-5 col-lg-3 col-form-label">地址 / Address</label>
                         <div class="col-sm-7 col-lg-9">
-                            <input type="text" class="form-control" id="inputAddress" name="address" placeholder="請輸入地址" />
+                            <input type="text" class="form-control" id="inputAddress" name="address" value="<?php echo $row[address] ?>" placeholder="請輸入地址" />
                         </div>
                     </div>
 
@@ -68,21 +68,28 @@
 </main>
 
 <?php
-    $sql_mail = "SELECT * FROM `members` where `email`='$_POST[email]'";
-    $result_mail = mysqli_query($db_link, $sql_mail) or die("查詢失敗");
-    $row_mail = mysqli_fetch_assoc($result_mail);
-    $mail_check = $row_mail['email'];
+    $sql_check = "SELECT * FROM `members` where `email`='$_POST[email]'";
+    $result_check = mysqli_query($db_link, $sql_check) or die("查詢失敗");
+    $row_check = mysqli_fetch_assoc($result_check);
+    $data_num = mysqli_num_rows($result_check);
 
     if (isset($_POST["save"])) {
         if ($_POST["name"] == null || $_POST["sex"] == null || $_POST["email"] == null || $_POST["telephone"] == null || $_POST["address"] == null) {
             echo "<script>alert('請輸入必填欄位');location.href='member-edit.php'</script>";
         }
-        elseif ($mail_check == $_POST['email']) {
-            echo "<script>alert('Email已被使用請重新輸入');location.href='member-edit.php'</script>";
+        elseif ($data_num != 0) {
+            if ($row_check["account"] != $row["account"]) {
+                echo "<script>alert('Email已被使用，請重新輸入');location.href='member-edit.php'</script>";
+            }
+            elseif ($row_check["account"] == $row["account"]) {
+                $sql_update = "UPDATE members SET `name` = '$_POST[name]', `gender` = '$_POST[sex]', `email` = '$_POST[email]', `address` = '$_POST[address]', `telephone` = '$_POST[telephone]' WHERE account = '$row[account]'";
+                mysqli_query($db_link, $sql_update);
+                echo "<script>alert('修改完成！');location.href='member.php'</script>";
+            }
         }
         else {
-            $sql_insert = "INSERT INTO `members` (name,gender,email,address,telephone) VALUES ('$_POST[name]','$_POST[sex]','$_POST[email]','$_POST[address]','$_POST[telephone]')";
-            mysqli_query($db_link, $sql_insert);
+            $sql_update = "UPDATE members SET `name` = '$_POST[name]', `gender` = '$_POST[sex]', `email` = '$_POST[email]', `address` = '$_POST[address]', `telephone` = '$_POST[telephone]' WHERE account = '$row[account]'";
+            mysqli_query($db_link, $sql_update);
             echo "<script>alert('修改完成！');location.href='member.php'</script>";
         }
     }
